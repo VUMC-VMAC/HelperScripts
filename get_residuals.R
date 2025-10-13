c1572aafbc02bb8c64c20513651907b721d5a6b8
ermahoney23 <emily.r.mahoney@vumc.org>
2025-10-13 18:30:34 -0400

    adding the get_residuals script

---
+++ get_residuals.R
@@ -0,0 +1,14 @@
+library(MASS)
+
+get_residuals <- function(namedataset,lmoutput,residual_ouput_variable_name){
+  
+  temp <- data.frame(row = as.numeric(names(lmoutput$residuals)), output_name = as.numeric(stdres(lmoutput)))
+  temp <- merge(temp, name_dataset,by="row",all.x=TRUE)
+  temp <- temp[c(3,2)]
+  names(temp)[2] <- residual_ouput_variable_name[1]
+	
+	return(temp)
+}
+
+	
+
